65b11bce72ee23ce3614d975c5ec5112152e5821
Adrian Hardt <Adrian@CaptainGeo.(none)>
2014-07-16 18:47:15 -0700

    Simplified:wq

--- RFID/testRFID.cpp
+++ RFID/testRFID.cpp
@@ -1,8 +1,7 @@
-/*
- • Project: A draft for an RFID input based attendance program. 
- • Author(s): Adrian Hardt and Logan Traffas.
- • Description: A program that will take an input identification from an RFID tag and set up a sign in time and sign out time for the person.
-*/
+// • Project: A draft for an RFID input based attendance program. 
+// • Author(s): Adrian Hardt and Logan Traffas. FIRST Team #1425 "ERROR CODE XERO".
+// • Description: A program that will take an input identification from an RFID tag and input it into a log.
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -10,48 +9,7 @@
 
 using namespace std;
 
-string setTheDay(){//Sets the day the people signed in.
-	bool outputDay=1;
-	string day;
-	string dayOfSigningIn;
-	time_t beginningDay;
-	struct tm* startDay;
-	time(&beginningDay);
-	startDay=localtime(&beginningDay);
-	dayOfSigningIn=asctime(startDay);
-	dayOfSigningIn.replace(24,2,"");
-	dayOfSigningIn.replace(10,9, "");
-	day=dayOfSigningIn;
-	string findDay;
-	ifstream searchForDay;
-	searchForDay.open("peopleList.txt");
-	while(!searchForDay.eof()){
-		string line;
-		getline(searchForDay,line);
-		char firstCharOfLine=line[0];
-		string findDay=line.substr(11,day.length());
-		if(firstCharOfLine=='O'){
-			if(day!=findDay){
-				ofstream peopleList;
-				peopleList.open("peopleList.txt",ios::app);
-				peopleList<<"\r\n"<<"Of the day "<<day<<": ";
-				peopleList.close();
-				outputDay=0;
-				break;
-			}
-			else{
-				outputDay=0;
-			}
-		}
-	}
-	if(outputDay==1){
-		ofstream peopleList;
-		peopleList.open("peopleList.txt",ios::app);
-		peopleList<<"\r\n"<<"Of the day "<<day<<": ";
-		peopleList.close();
-	}
-	searchForDay.close();
-	return day;
+void memberRead(string name){//Reads out (from the log) the time the person was present.
 }
 
 string inTime(){//Sets the time the people signed in.
@@ -61,38 +19,24 @@ string inTime(){//Sets the time the people signed in.
 	time(&beginningTime);
 	startTime=localtime(&beginningTime);
 	signInTime=asctime(startTime);
-	signInTime.replace(24,2,"");
-	signInTime.replace(0,11,"");
-	signInTime.replace(8,5,"");
+	signInTime.erase(24,2);
+	string moveYear=signInTime.substr(19,5);
+	signInTime.erase(19,5);
+	signInTime.insert(10,moveYear);
 	return signInTime;
-}
-
-string outTime(){//Sets the time the people signed out.
-	string signOutTime;
-	time_t finishTime;//Sets the time for when the person signed out.
-	struct tm* endTime;
-	time(&finishTime);
-	endTime=localtime(&finishTime);
-	signOutTime=asctime(endTime);
-	signOutTime.replace(24,2,"");
-	signOutTime.replace(0,11,"");
-	signOutTime.replace(8,5,"");
-	return signOutTime;
 }
 
 string getName(string RFIDin){//Sets the name of the person signing in.
 	string name;
-	string tempName;
 	string tempId;
 	ifstream inputName;
 	inputName.open("IdAndNames.txt");
-	while(!inputName.eof()){
+	while(!(inputName.eof())){
 		string line;
-		string addLetter;
-		unsigned int findEqualSign=0;
-		string equalSign;
 		getline(inputName,line);
 		tempId=line;
+		string equalSign;
+		unsigned int findEqualSign=0;
 		for(findEqualSign; findEqualSign<=tempId.length(); findEqualSign++){
 			equalSign=tempId[findEqualSign];
 			if(equalSign=="="){
@@ -104,17 +48,22 @@ string getName(string RFIDin){//Sets the name of the person signing in.
 		tempId.replace(findEqualSign,tempIdLength,"");
 		unsigned int locationOfLetter=findEqualSign+1;
 		if(RFIDin==tempId){
+			string tempName;
 			tempName=line;	
 			tempName.replace(0,locationOfLetter,"");
+			string addLetter;
 			for(locationOfLetter; locationOfLetter<=tempName.length(); locationOfLetter++){
 				addLetter=tempName[locationOfLetter];
 				name.append(addLetter);
 			}
+			name=tempName;
+			name.replace(name.length()-1,2,"");
 			break;
 		}
 	}
+	inputName.close();
 	if(RFIDin!=tempId){
-		cout<<"That id is not associated with a name yet. Please add a name to associate with the id "<<RFIDin<<"?: ";
+		cout<<endl<<"That id is not associated with a name yet. Please add a name to associate with the id \""<<RFIDin<<"\": ";
 		string nameToAdd;
 		while(nameToAdd==""){
 			getline(cin,nameToAdd);
@@ -123,88 +72,30 @@ string getName(string RFIDin){//Sets the name of the person signing in.
 		addNewName.open("IdAndNames.txt",ios::app);
 		addNewName<<"\r\n"<<RFIDin<<"="<<nameToAdd;
 		addNewName.close();
+		name=nameToAdd;
 	}
-	name=tempName;
-	name.replace(name.length()-1,2,"");
-	inputName.close();
 	return name;
 }
 
-void outputAttendance(string name, string signOutTime, string signInTime, string signInOrOut, string day){//Outputs the attendance to the corresponding file.
-	if(signInOrOut=="1"){
-		ofstream peopleList;
-		peopleList.open("peopleList.txt",ios::app);
-		peopleList<<"\r\n        • "<<name<<" was here from: "<<signInTime<<" to: ";
-		peopleList.close();
-	}
-	else if(signInOrOut=="2"){
-		//ofstream peopleList;
-		//peopleList.open("peopleList.txt",ios::app);
-		//peopleList<<signOutTime;
-		//peopleList.close();
-	}
+void outputAttendance(string RFIDin, string name, string signInTime){//Outputs the attendance to the corresponding file.
+	ofstream logOfTimes;
+	logOfTimes.open("logOfTimes.txt",ios::app);
+	logOfTimes<<name<<"="<<RFIDin<<"="<<signInTime<<"\r\n";
+	logOfTimes.close();
 }
 
-int main(){
-	bool setDay=1;
-	
-	while(true){//Input the RFID.
-		string signInOrOut="0";
-		cout<<"Enter \"1\" to sign in, enter \"2\" to sign out, or enter \"3\" to exit: ";
-		cin>>signInOrOut;
-		if(signInOrOut=="3"){
-			break;
-		}
-		else if(signInOrOut!="1" && signInOrOut!="2"){
-			break;
-		}
-		else{
-			string day;
-			string name;
-			string RFIDin;
-			string signInTime;
-			string signOutTime;
-			if(setDay==1){
-				setTheDay();
-				setDay=0;
-			}
-			bool inputId=0;
-			while(signInOrOut=="1"){//Signing in RFID input.
-				if(inputId==1){
-					break;
-				}
-				else{
-					cout<<"You are signing in. What is your id? ";
-					cin>>RFIDin;
-					getName(RFIDin);
-					cout<<endl;
-					if(inputId!=1){
-						name=getName(RFIDin);
-						signInTime=inTime();
-						signOutTime="";
-						outputAttendance(name, signOutTime, signInTime, signInOrOut, day);
-						inputId=1;
-					}
-				}
-			}
-			inputId=0;
-			while(signInOrOut=="2"){//Signing out RFID input.
-				string RFIDin;
-				cout<<"You are signing out. What is your id? ";
-				cin>>RFIDin;
-				cout<<endl;
-				if(inputId!=1){
-					name=getName(RFIDin);
-					signInTime=inTime();
-					signOutTime=outTime();
-					outputAttendance(name, signOutTime, signInTime, signInOrOut, day);
-					inputId=1;
-				}
-				else if(inputId==1){
-					break;
-				}
-			}
-		}
+int main(){//Input the RFID.
+	while(1){
+		string name;
+		string RFIDin;
+		string signInTime;
+		cout<<"Please input your id.(Input 0 to exit): ";
+		cin>>RFIDin;
+		if(RFIDin=="0")break;
+		cout<<endl;
+		name=getName(RFIDin);
+		signInTime=inTime();
+		outputAttendance(RFIDin, name, signInTime);
 	}
 	return 0;
 }
