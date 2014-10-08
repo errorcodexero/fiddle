fb964dd556baed0d8b3d88ec0cfaacce0ab2df95
Jacob Tiderman <Jacob@Jacob-PC.(none)>
2014-10-07 20:51:54 -0700

    Code now solves for the rotation of the "robot", and also calculates the distance from the robot's starting point on an x,y grid.

--- Swerve/D_t.cpp
+++ Swerve/D_t.cpp
@@ -21,7 +21,13 @@ void tableDisplay(){
 	float stepsize = .25;
 	float ltotal = 0;
 	float rtotal = 0;
-	cout<<"Time"<<"		"<<"LDistance"<<"	"<<"LVelocity"<<"	"<<"LTotal"<<"			"<<"RDistance"<<"	"<<"RVelocity"<<"	"<<"RTotal"<<"\n";
+	float theta = 0; //angle of turn
+	float delta_theta = 0; //angle increase in turn
+	float delta_y; //difference in distance travelled between wheels
+	float distance_between_wheels = 3; //example: 3ft between left and right wheels
+	float x_coord = 0;
+	float y_coord = 0;
+	cout<<"Time"<<"		"<<"LDistance"<<"	"<<"LVelocity"<<"	"<<"LTotal"<<"			"<<"RDistance"<<"	"<<"RVelocity"<<"	"<<"RTotal"<<"		"<<"Theta"<<"		"<<"coordinates"<<"\n";
 	while(count != 20){
 		float lvelocity = sin(time);
 		float rvelocity = cos(time);
@@ -29,11 +35,23 @@ void tableDisplay(){
 		rdistance += stepsize * rvelocity;
 		ltotal += fabs(stepsize *lvelocity);
 		rtotal += fabs(stepsize *rvelocity);
-		cout<<f(time)<<"		"<<f(ldistance)<<"		"<<f(lvelocity)<<"		"<<f(ltotal)<<"			"<<f(rdistance)<<"		"<<f(rvelocity)<<"		"<<f(rtotal)<<"\n";
+		delta_y = ltotal - rtotal;
+		delta_theta = atan2(delta_y, distance_between_wheels);
+		theta += delta_theta;
+		float h = (ltotal + rtotal)/2;
+		x_coord = (sin(theta))*h;
+		y_coord = (cos(theta))*h;
+		
+		
+		
+		
+		cout<<f(time)<<"		"<<f(ldistance)<<"		"<<f(lvelocity)<<"		"<<f(ltotal)<<"			"<<f(rdistance)<<"		"<<f(rvelocity)<<"		"<<f(rtotal)<<"		"<<f(theta)<<"		"<<f(x_coord)<<", "<<f(y_coord)<<"\n";
 		time=time+stepsize;
 		count++;
 	}
 }
+
+
 
 
 int main(){
