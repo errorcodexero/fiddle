7e0882e546d18f0451df0468ccca30e240e085cb
Logan Traffas <logan.traffas@gmail.com>
2014-12-01 20:10:09 -0800

    Updated version

--- Swerve/swerve_rotation_control.cpp
+++ Swerve/swerve_rotation_control.cpp
@@ -2,8 +2,11 @@
 #include <iostream>
 #include <cmath>
 #include <math.h>
+#include <vector>
 
-//test words pls ignore
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 using namespace std;
 
@@ -19,6 +22,9 @@ double determine_change(double current_rotation, double target_rotation){//Deter
 	else if(target_rotation-current_rotation<=M_PI && target_rotation-current_rotation>-M_PI)return (target_rotation-current_rotation);//Determine the amount of radians needed to change
 	else if(target_rotation-current_rotation>M_PI)return ((target_rotation-current_rotation)-(2*M_PI));//Determine the amount of radians needed to change
 	else if(target_rotation-current_rotation<-M_PI)return ((target_rotation-current_rotation)+(2*M_PI));//Determine the amount of radians needed to change
+	else{
+		return 0;
+	}
 }
 
 double correct_rotating_form(double current_rotation){//During wheel turn, this will correct to radian form (i.e. 0-(2*pi))
@@ -34,7 +40,7 @@ double simulation(double current_rotation, double target_rotation, double radian
 		if(i==-1)i++;
 		if(i==0)cout<<"At "<<current_rotation<<" radians."<<endl;//Prints the starting rotation
 		if(radian_change<0){//Negative change
-			decimal_difference=target_rotation-current_rotation;//Sets and corrects the difference between current and target rotations for fractional calculation
+			decimal_difference=target_rotation-current_rotation;//Sets and corrects the difference of current and target rotations for fractional calculation
 			if(decimal_difference>M_PI)decimal_difference=target_rotation-current_rotation-(2*M_PI);//Ensures it will calculate in the right direction
 			if(decimal_difference>-1 && decimal_difference<0){//Changes wheel rotation for cases when the difference is less than one radian.
 				current_rotation=target_rotation;
@@ -46,7 +52,7 @@ double simulation(double current_rotation, double target_rotation, double radian
 			cout<<"At "<<current_rotation<<" radians."<<endl;
 		}
 		else if(radian_change>0){//Positive change
-			decimal_difference=correct_radian_form(target_rotation-current_rotation);//Sets and corrects the difference between current and target rotations for fractional calculation 
+			decimal_difference=correct_radian_form(target_rotation-current_rotation);//Sets and corrects the difference of current and target rotations for fractional calculation 
 			if(decimal_difference>0 && decimal_difference<1){//Changes wheel rotation for cases when the difference is less than one radian.
 				current_rotation=target_rotation;
 				cout<<"At "<<current_rotation<<" radians."<<endl;
@@ -60,21 +66,68 @@ double simulation(double current_rotation, double target_rotation, double radian
 	return current_rotation;
 }
 
-void current_rotation_direction(){//Uses the order of channel outputs for pins two (a) and four (b) to determine wheel rotating direction
-	char leader; 
-	bool rotating_direction;
-	cout<<"What is the leading channel? ";
-	cin>>leader;
-	cout<<endl;
-	if(leader=='a')cout<<"Wheel currently rotating clockwise."<<endl;
-	else if(leader=='b')cout<<"Wheel currently rotating counter-clockwise."<<endl;
-	cout<<endl;
+struct Input{//The type of input received from the encoder -- "a" (pin two) and "b" (pin four)
+	bool a,b;
+};
+
+ostream & operator<<(ostream & o, Input in){//Outputs from the vector if type Input the states of the a and b channels as this code interprets it
+	o<<in.a<<" | "<<in.b;
+	return o;
+}
+
+void current_rotation_direction(){//Uses the order of channel outputs for pins two (a) and four (b) to determine wheel rotating direction and estimates the amount of rotational change
+	double estimated_rotation=0;
+	cout<<"time | a | b | estimated rotation"<<endl;
+	vector<Input> time;
+	Input channel_value;
+	channel_value.a=0;
+	channel_value.b=0;//t=0
+	time.push_back(channel_value);
+	channel_value.a=1;
+	channel_value.b=0;//t=1
+	time.push_back(channel_value);
+	channel_value.a=1;
+	channel_value.b=1;//t=2
+	time.push_back(channel_value);
+	channel_value.a=0;
+	channel_value.b=1;//t=3
+	time.push_back(channel_value);
+	channel_value.a=0;
+	channel_value.b=0;//t=4
+	time.push_back(channel_value);
+	channel_value.a=1;
+	channel_value.b=0;//t=5
+	time.push_back(channel_value);
+	channel_value.a=1;
+	channel_value.b=1;//t=6
+	time.push_back(channel_value);
+	channel_value.a=0;
+	channel_value.b=1;//t=7
+	time.push_back(channel_value);
+	channel_value.a=0;
+	channel_value.b=0;//t=8
+	time.push_back(channel_value);
+	channel_value.a=1;
+	channel_value.b=0;//t=9
+	time.push_back(channel_value);
+	for(unsigned int i=0;i<time.size(); i++){//TURNING DOESN'T WORK YET (i.e. cannot really simulate/interpret turning)!! FIXING IS TOP PRIORITY
+		if(time[i].a==time[i].b){
+			//Null (i.e it assumes there was no change -- at least, for now...)
+		}
+		else if(((time[i].a==1 && time[i].b==0) && (time[i-1].a==0 && time[i-1].b==0)) || ((time[i].a==0 && time[i].b==1) && (time[i-1].a==1 && time[i-1].b==1))){//Determines if wheel is rotating clockwise
+			estimated_rotation+=M_PI;
+		}
+		else if(((time[i].a==0 && time[i].b==1) && (time[i-1].a==0 && time[i-1].b==0)) || ((time[i].a==1 && time[i].b==0) && (time[i-1].a==1 && time[i-1].b==1))){//Determines if wheel is rotating counter-clockwise
+			estimated_rotation-=M_PI;
+		}
+		cout<<"  "<<i<<"  | "<<time[i]<<" | "<<estimated_rotation<<endl;
+	}
 }
 
 int main(){
 	current_rotation_direction();
-	double current_rotation, target_rotation, radian_change, decimal_difference;
-	cout<<"What is the current wheel rotation? ";//Prompts the user for current wheel rotation
+	/*double current_rotation, target_rotation, radian_change;
+	cout<<endl<<"What is the current wheel rotation? ";//Prompts the user for current wheel rotation
 	cin>>current_rotation;
 	current_rotation=correct_radian_form(current_rotation);
 	cout<<"What is the target wheel rotation? ";//Prompts the user for current wheel rotation
@@ -82,6 +135,6 @@ int main(){
 	cout<<endl;
 	target_rotation=correct_radian_form(target_rotation);
 	radian_change=determine_change(current_rotation, target_rotation);
-	current_rotation=simulation(current_rotation, target_rotation, radian_change);//Returns current_rotation after simulation
+	current_rotation=simulation(current_rotation, target_rotation, radian_change);//Returns current_rotation after simulation*/
 	return 0;
 }
