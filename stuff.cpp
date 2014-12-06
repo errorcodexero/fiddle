71fda742f09cc63473ba2f5920c07781756a11be
frc1425 <frc1425@minime.(none)>
2014-12-06 14:51:03 -0800

    stuff.cpp update

---
+++ stuff.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+
+int main(){
+	string test;
+	test = "Hi There";
+	cout <<test<<"\n";
+
+	float a, b;
+	float result;	
+	
+	a = 5.3;
+	b = 5.7;
+	
+	result = a * b;
+	cout<<result<<"\n";
+	return 0;
+}
