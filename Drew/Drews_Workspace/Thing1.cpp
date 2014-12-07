cfaf9e04d124dd26053a846aae41d2e30a27a915
frc1425 <frc1425@minime.(none)>
2014-12-06 16:00:26 -0800

    Updated File

---
+++ Drew/Drews_Workspace/Thing1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	float num1, num2
+	int resukt
+	float num1 = 5.4;
+	float num2 = 5.3;
+	result = num1 * num2;
+	cout<<result<<"\n";
+}
