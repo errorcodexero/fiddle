e410fcc3b0a5302bee52a8d2402b7b119ce39628
mchenrycalvin@gmail.com <mchenrycalvin@gmail.com>
2014-12-09 20:45:51 -0800

    Commiting Drew's changes to his code in "Drew" the folder. -Jacob

--- Drew/Drews_Workspace/Thing1.cpp
+++ Drew/Drews_Workspace/Thing1.cpp
@@ -2,11 +2,25 @@
 #include<string>
 using namespace std;
 
-int main(){
-	float num1, num2
-	int resukt
-	float num1 = 5.4;
-	float num2 = 5.3;
-	result = num1 * num2;
-	cout<<result<<"\n";
+int main(/*int answer, char** argv*/){
+	string answer;
+	cout<<"What is the color of money?\n";
+	cin>>answer;
+	cout<<"\n";
+	if (answer == "Green"){
+		cout<<"Correct, Money is green\n";
+	}
+	else{ cout<<"No that is not the color of money\n";
+	}
+	
+	/*int result1 = 5 + 7;
+	cout<<result1<<"\n";
+	if (result1 == 12){
+		string result2 = "5 + 7 = 12 Yay, it is correct!";
+		cout<<result2<<"\n";
+	} 
+	else{
+		string result3 = "Not Correct, 5 + 7 = 12 :(" ;
+		cout<<result3<<"\n";
+	}*/
 }
