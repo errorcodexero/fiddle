27cd0d2c88947c8b1f82a92a7d034d22a6728377
Ibrahim <ibrahimibro99@gmail.com>
2015-01-30 08:35:27 -0800

    Ibrahim updated liftHeight.cpp/h

--- liftHeight.cpp
+++ liftHeight.cpp
@@ -5,50 +5,36 @@
 using namespace std; 
 
 int intCan(string c) {
-	int can = 0;
-	
-	if (c[0] == 'Y' || c[0] == 'y') {
-		can = 1;
-	}
-	
-	return can;
+	return (c[0] == 'Y' || c[0] == 'y') ? 1 : 0;
 }
 
 int intBase(string b) {
 	Height base;
-	int height = 0;
 	
 	if (b[0] == 'G' || b[0] == 'g') {
-		height = base.ground;
+		return base.ground;
 	}
 	else if (b[0] == 'P' || b[0] == 'p') {
-		height = base.platform;
+		return base.platform;
 	}
 	else if (b[0] == 'S' || b[0] == 's') {
-		height = base.step;
+		return base.step;
 	}
-	
-	return height;
 }
 
 int main(){
-	int nTote = 0,
-		liftHeight;
-	string base,
-		   can;
+	int nTote = 0;
+	string base = "",
+		   can = "";
 	
 	cout << endl << "Enter number of totes (0-3): ";
 	cin >> nTote;
-	
 	cout << "Enter the base (Ground/Platform/Step): ";
 	cin >> base;
-	
 	cout << "Is can on the stack? (Y/N) ";
 	cin >> can;
 	
-	liftHeight = getLiftHeight(nTote, intBase(base), intCan(can));
-	
-	cout << endl << "Lift height will be " << liftHeight << " centimeters.";
+	cout << endl << "Lift height will be " << getLiftHeight(nTote, intBase(base), intCan(can)) << " centimeters.";
 	
 	return 0;
 }
--- liftHeight.h
+++ liftHeight.h
@@ -1,29 +1,21 @@
 #include <iostream>
 
-#define MaxLiftHeight 182.88 //six feet
-#define MinLiftHeight 5.0 //minimum lift height above ground
-#define MaxLevel 15.875 + (double(3) * 30.734) + 73.66 //step + 3 totes + can
-#define MinLevel 0.0 //ground
+#define MAX_LIFT_HEIGHT 182.88 //six feet
+#define MIN_LIFT_HEIGHT 5.0 //minimum lift height above ground
+#define MAX_LEVEL 15.875 + (double(3) * 30.734) + 73.66 //step + 3 totes + can
+#define MIN_LEVEL 0.0 //ground
 
-using namespace std; 
 
-class Height { //centimeters
-	public:
-		double ground = 0,
-			   platform = 5.08,
-			   step = 15.875,
-			   tote = 30.734,
-			   can = 73.66,
-			   toteTolerance = 10,  //tolerance from the top of the tote
-			   canTolerance = 30;   //tolerance from the top of the can
-	
-	private:
+struct Height { //centimeters
+	const double ground = 0,
+		   platform = 5.08,
+		   step = 15.875,
+		   tote = 30.734,
+		   can = 73.66,
+		   toteTolerance = 10,  //tolerance from the top of the tote
+		   canTolerance = 30;   //tolerance from the top of the can
 		
 };
-
-int mapToLiftLevel(int level, int minLevel, int maxLevel, int minLiftHeight, int maxLiftHeight) {
-	return (level - minLevel) * (maxLiftHeight - minLiftHeight) / (maxLevel - minLevel) + minLiftHeight;
-}
 
 int mapToLiftLevel(int level, double minLevel, double maxLevel, double minLiftHeight,double maxLiftHeight) {
 	return (level - minLevel) * (maxLiftHeight - minLiftHeight) / (maxLevel - minLevel) + minLiftHeight;
@@ -37,5 +29,5 @@ int getLiftHeight(int nTote, int base, int can = 0) {
 		heightLevel -= height.toteTolerance;
 	}
 	
-	return mapToLiftLevel(heightLevel, MinLevel, MaxLevel, MinLiftHeight, MaxLiftHeight);
+	return mapToLiftLevel(heightLevel, MIN_LEVEL, MAX_LEVEL, MIN_LIFT_HEIGHT, MAX_LIFT_HEIGHT);
 }
