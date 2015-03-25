00cfdbeb598b3283736f3bbb2287b561f7b8d990
rince_charming <nothing@nothin.com>
2015-03-24 19:48:16 -0700

    "server" folder update

--- server/server.c
+++ server/server.c
@@ -7,6 +7,7 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 void error(const char *msg)
 {
@@ -14,14 +15,15 @@ void error(const char *msg)
     exit(1);
 }
 
-int main(int argc, char *argv[])
+int main(/*int argc, char *argv[]*/)
 {
      int sockfd, newsockfd, portno;
+     int portnumber = 6666;
      socklen_t clilen;
      char buffer[65536+1];
      struct sockaddr_in serv_addr, cli_addr;
      int n;
-     if (argc < 2) {
+     if (portnumber/*argc*/ < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
@@ -29,7 +31,7 @@ int main(int argc, char *argv[])
      if (sockfd < 0) 
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
+     portno = portnumber; //atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
      serv_addr.sin_port = htons(portno);
