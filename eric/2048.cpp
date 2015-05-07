2a1a6e43393d355b1a4679103fa0a08641e3eea0
E Rippey <rippey.e@gmail.com>
2015-05-06 22:06:59 -0700

    Make 2048 work

--- eric/2048.cpp
+++ eric/2048.cpp
@@ -3,26 +3,35 @@
 #include<set>
 #include<vector>
 #include<type_traits>
+#include<array>
 
 using namespace std;
 
 #define nyi { cout<<"nyi "<<__LINE__<<"\n"; exit(44); }
 
-//vector<T> to_vector(Collection)nyi
+template<typename T>
+vector<T>& operator|=(vector<T>& v,T t){
+	v.push_back(t);
+	return v;
+}
 
-/*template<typename Func,typename Collection>
-auto filter(Func f,Collection v)->vector<
-	std::remove_reference<
-		decltype(*(v.begin()))
-	>::type
->{
-	/*vector<decltype(*v.begin())> r;
-	for(auto a:v){
-		if(f(a)) r|=a;
-	}
-	return r;*/
-/*	nyi
-}*/
+template<typename T>
+set<T>& operator|=(set<T>& s,T t){
+	s.insert(t);
+	return s;
+}
+
+vector<unsigned> range(unsigned a){
+	vector<unsigned> r;
+	for(unsigned i=0;i<a;i++) r|=i;
+	return r;
+}
+
+template<typename T>
+T choose(vector<T> v){
+	assert(v.size());
+	return v[rand()%v.size()];
+}
 
 typedef int Piece;
 
@@ -36,7 +45,13 @@ Piece new_piece(){
 typedef pair<int,int> Location;
 
 set<Location> locations(){
-	nyi
+	set<Location> r;
+	for(auto x:range(4)){
+		for(auto y:range(4)){
+			r|=pair<int,int>(x,y);
+		}
+	}
+	return r;
 }
 
 Location random_location(){
@@ -58,28 +73,144 @@ struct Board{
 		return data[l.first][l.second];
 	}
 
+	Piece operator[](Location l)const{
+		return data[l.first][l.second];
+	}
+
 	Piece *begin()nyi
 	Piece *end()nyi
 };
 
-ostream& operator<<(ostream& ,Board const& ){
-	nyi
+ostream& operator<<(ostream& o,Board const& b){
+	for(auto row:range(4)){
+		for(auto col:range(4)){
+			o<<b[pair<int,int>(row,col)]<<"\t";
+		}
+		o<<"\n";
+	}
+	return o;
+}
+
+bool operator==(Board const& a,Board const& b){
+	for(auto l:locations()){
+		if(a[l]!=b[l]) return 0;
+	}
+	return 1;
+}
+
+bool operator!=(Board const& a,Board const& b){
+	return !(a==b);
 }
 
 enum class Move{LEFT,RIGHT,UP,DOWN};
 
-Move random_move()nyi
+static const vector<Move> MOVES{Move::LEFT, Move::RIGHT, Move::UP, Move::DOWN};
 
-Board move(Board b, Move m){
-	nyi
+ostream& operator<<(ostream& o,Move a){
+	#define X(NAME) if(a==Move::NAME) return o<<""#NAME;
+	X(LEFT) X(RIGHT) X(UP) X(DOWN)
+	#undef X
+	assert(0);
 }
 
+Move random_move(){
+	return choose(MOVES);
+}
 
+array<Piece,4> collapse(array<Piece,4> a){
+	vector<Piece> used;
+	for(auto elem:a){
+		if(elem) used|=elem;
+	}
+	vector<Piece> after;
+	for(unsigned i=0;i<used.size();i++){
+		bool collapse=i+1<used.size() && used[i]==used[i+1];
+		if(collapse){
+			after|=used[i]*2;
+			i++;
+		}else{
+			after|=used[i];
+		}
+	}
+	array<Piece,4> r;
+	for(auto& a:r) a=0;
+	for(unsigned i=0;i<after.size();i++) r[i]=after[i];
+	return r;
+}
+
+Board left(Board b){
+	Board r;
+	for(unsigned row:range(4)){
+		array<Piece,4> this_row;
+		for(unsigned col=0;col<4;col++){
+			auto l=make_pair(row,col);
+			this_row[col]=b[l];
+		}
+		auto after=collapse(this_row);
+		for(unsigned col=0;col<4;col++){
+			r[make_pair(row,col)]=after[col];
+		}
+	}
+	return r;
+}
+
+Board right(Board b){
+	Board r;
+	for(unsigned row:range(4)){
+		array<Piece,4> this_row;
+		for(auto col:range(4)){
+			this_row[3-col]=b[make_pair(row,col)];
+		}
+		auto after=collapse(this_row);
+		for(auto col:range(4)){
+			r[make_pair(row,col)]=after[3-col];
+		}
+	}
+	return r;
+}
+
+Board up(Board b){
+	Board r;
+	for(auto col:range(4)){
+		array<Piece,4> a;
+		for(auto row:range(4)){
+			a[row]=b[make_pair(row,col)];
+		}
+		auto after=collapse(a);
+		for(auto row:range(4)){
+			r[make_pair(row,col)]=after[row];
+		}
+	}
+	return r;
+}
+
+Board down(Board b){
+	Board r;
+	for(auto col:range(4)){
+		array<Piece,4> a;
+		for(auto row:range(4)){
+			a[3-row]=b[make_pair(row,col)];
+		}
+		auto after=collapse(a);
+		for(auto row:range(4)){
+			r[make_pair(row,col)]=after[3-row];
+		}
+	}
+	return r;
+}
+Board move(Board b, Move m){
+	switch(m){
+		case Move::LEFT: return left(b);
+		case Move::RIGHT: return right(b);
+		case Move::UP: return up(b);
+		case Move::DOWN: return down(b);
+		default: assert(0);
+	}
+}
 
 bool game_over(Board const& b){
-	vector<Move> moves {Move::LEFT, Move::RIGHT, Move::UP, Move::DOWN};
-	for(int i = 0; i < moves.size(); i++){
-		if(move(b,moves[i]) != b){
+	for(unsigned i = 0; i < MOVES.size(); i++){
+		if(move(b,MOVES[i]) != b){
 			return false;
 		}
 	}
@@ -109,13 +240,67 @@ Board new_game(){
 	return r;
 }
 
-Board apply(Board const&,Move)nyi
-
-int main(){
-	Board b=new_game();
-	while(!game_over(b)){
-		cout<<b<<"\n";
-		b=apply(b,random_move());
+vector<Location> unoccupied(Board b){
+	vector<Location> r;
+	for(auto l:locations()){
+		if(!b[l]) r|=l;
 	}
+	return r;
 }
 
+Board add_random(Board b){
+	b[choose(unoccupied(b))]=new_piece();
+	return b;
+}
+
+template<typename Func>
+unsigned try_game(Func f,bool print){
+	Board b=new_game();
+	Board old=b;
+	unsigned moves=0;
+	while(!game_over(b)){
+		if(b!=old){
+			b=add_random(b);
+			moves++;
+		}
+		if(print)cout<<b<<"\n";
+		auto move1=f(b);
+		if(print)cout<<move1<<"\n";
+		old=b;
+		b=move(b,move1);
+	}
+	if(print)cout<<moves<<"\n";
+	return moves;
+}
+
+template<typename T>
+T sum(vector<T> v){
+	T r=0;
+	for(auto a:v) r+=a;
+	return r;
+}
+
+template<typename T>
+T mean(vector<T> v){
+	return sum(v)/v.size();
+}
+
+Move ai_random(Board){
+	return choose(MOVES);
+}
+
+Move upper_left(Board b){
+	if(move(b,Move::LEFT)!=b) return Move::LEFT;
+	if(move(b,Move::UP)!=b) return Move::UP;
+	if(move(b,Move::RIGHT)!=b) return Move::RIGHT;
+	return Move::DOWN;
+}
+
+int main(){
+	srand(time(NULL));
+	vector<unsigned> results;
+	for(unsigned i=0;i<1000;i++){
+		results|=try_game(ai_random,0);
+	}
+	cout<<mean(results)<<"\n";
+}
--- eric/2048.sh
+++ eric/2048.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 -Wall -Wextra 2048.cpp -o 2048 2>&1 && ./2048
+g++ -std=c++11 -Wall -Wextra -O3 2048.cpp -o 2048 2>&1 && ./2048
