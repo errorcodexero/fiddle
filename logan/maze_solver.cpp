0c15cf8d19f16ae7b07c7c704ea6b5389c2fae31
Logan Traffas <logan.traffas@gmail.com>
2015-07-02 23:27:49 -0700

    Made some functionality improvements and added an easier maze input method with maze.txt.

--- logan/maze_solver.cpp
+++ logan/maze_solver.cpp
@@ -1,24 +1,26 @@
 /*To do:
-	- 
+	- Make maze-making easier (auto-random generate) 
 */
 #include <iostream>
 #include <string>
 #include <utility>
 #include <vector>
 #include <assert.h>
+#include <fstream>
+#include <array>
+#include <cstdlib>
 
 using namespace std;
 
 typedef pair<int,int> Location;
+enum class Move{FORWARD, BACKWARD, RIGHT, LEFT};
 
-//Edit the following to change the maze:
+//Edit the following to change the default maze (Alternatively edit the accompanying maze.txt):
 static const int X_LIMIT=10, Y_LIMIT=10;
 static const Location TARGET_LOCATION=Location{0,5};
 static const Location SOLVER_ORIGIN=Location{0,0};
 static const vector<Location> BLOCKS={Location{0,3}};
 //End.
-
-enum class Move{FORWARD, BACKWARD, RIGHT, LEFT};
 
 ostream& operator<<(ostream& o, Move in){
 	#define X(name) if(in==Move::name)o<<""#name;
@@ -153,17 +155,18 @@ pair<bool,vector<Move>> find_path(Maze a, Maze b, int max=0){
 vector<Move> get_path(Maze a, Maze b){
 	vector<Move> path;
 	pair<bool,vector<Move>> path_return;
-	for(unsigned int i=0; i<1000; i++){
+	for(unsigned int i=0; i<100000000; i++){
 		path_return=find_path(a, b, i); 
 		if(path_return.first){
 			path=path_return.second;
 			return path;
 		}
 	} 
-	assert(0);
+	cout<<"\nWarning: Path length exceeds 100,000,000 -- This calculation could take a few more minutes to complete or an error may have occurred: line:"<<__LINE__<<"\n"; //100000000 is 
+	exit(44); 
 }
 
-void print_maze(const Maze a, vector<Move> path){
+void print_maze(const Maze a, vector<Move> path=vector<Move>{}){
 	vector<vector<char>> maze;
 	for(int j=0; j<a.y_lim+2; j++){//Makes a maze-sized print-out vector
 		maze.push_back(vector<char>{});
@@ -200,18 +203,69 @@ Maze make_goal(const Maze a){
 	return b;
 }
 
-int main(){
-	Maze maze;
-	maze.x_lim=X_LIMIT;
-	maze.y_lim=Y_LIMIT;
-	maze.blocks=BLOCKS;
-	maze.solver.loc=SOLVER_ORIGIN;
-	maze.target=TARGET_LOCATION;
-	cout<<"Welcome to this maze solver!\n('S' is the solver's origin. 't' is the target. 'X' is a barrier.)\n";
-	Maze goal=make_goal(maze);
-	vector<Move> path;
-	path=get_path(maze, goal);
-	print_maze(maze,path);
-	cout<<"The path used is:\n"<<path<<"\n";
+Maze import_maze(){
+	Maze a;
+	ifstream maze;
+	maze.open("maze.txt");
+	vector<string> lines;
+	int k=0;
+	while(!maze.eof()){
+		k++;
+		string line;
+		getline(maze,line);
+		for(unsigned int i=0; i<line.size(); i++){
+			if(line[i]=='\r')line.erase(line.begin()+i);
+		}
+		a.x_lim=line.size();
+		lines.push_back(line);
+	}
+	if(k==0)cout<<"File \"maze.txt\" does not contain a maze\n\n";
+	else{
+		a.y_lim=k;
+		for(int i=0; i<a.y_lim; i++){
+			string line=lines[a.y_lim-1-i];
+			for(int j=0; j<a.x_lim; j++){
+				if(line[j]=='X')a.blocks.push_back(Location{j,i});
+				if(line[j]=='S')a.solver.loc=Location{j,i};
+				if(line[j]=='T')a.target=Location{j,i};	
+			}
+		}
+}
+	maze.close();
+	Maze b;
+	return b;
+}
+
+int main(int x,char *import[]){
+	if(x!=2)cout<<"\nInvalid usage (check: "<<import[0]<<" --options)";
+	else{
+		char import_cmp[]={"--import"};
+		char options_cmp[]={"--options"};
+		char default_cmp[]={"--default"};
+		Maze maze;
+		if(string(import[1])==string(default_cmp)){
+			cout<<"Welcome to this maze solver!\n('S' is the solver's origin. 'T' is the target. 'X' is a barrier.)\n\n";
+			maze.x_lim=X_LIMIT;
+			maze.y_lim=Y_LIMIT;
+			maze.blocks=BLOCKS;
+			maze.solver.loc=SOLVER_ORIGIN;
+			maze.target=TARGET_LOCATION;
+			Maze goal=make_goal(maze);
+			vector<Move> path;
+			path=get_path(maze, goal);
+			print_maze(maze,path);
+			cout<<"\nThe path used is:\n"<<path<<"\n";
+		}
+		else if(string(import[1])==string(import_cmp)){
+			cout<<"Welcome to this maze solver!\n('S' is the solver's origin. 'T' is the target. 'X' is a barrier.)\n\n";
+			print_maze(import_maze(),get_path(import_maze(),make_goal(import_maze())));
+			cout<<"\nThe path used is:\n"<<get_path(import_maze(),make_goal(import_maze()))<<"\n";
+		}
+		else if(string(import[1])==string(options_cmp)){
+			cout<<import[0]<<" can be used with the following flags:\n        --default\n                To run the maze solver on the built-in maze (written in the code)\n \
+			--import\n                To run the maze solver on an imported maze from maze.txt";
+		}
+		else cout<<import[0]<<": error: no such option: "<<import[1]<<"\n";
+	}
 	return 0;
 }
