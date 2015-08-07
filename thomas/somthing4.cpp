c214ef5458ef5ca46abd7254af3219f42b7d95dd
Wonderwoman <nothing@nothin.com>
2015-08-06 20:28:45 -0700

    save 8-6-15

--- thomas/somthing4.cpp
+++ thomas/somthing4.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main(){
+	vector<pair<int,int>> v{
+		{10,11}, //0
+		{11,10}, //1
+		{11,12}, //2
+		{12,11}, //2
+		{11,15}, //3
+		{15,11}, //4
+		{13,12}, //5
+		{12,13}, //6
+		{16,12}, //7
+		{12,16}, //8
+		{16,15}, //9
+		{15,16}, //10
+		{17,16}, //11
+		{16,17}, //12
+		{14,15}, //13
+		{15,14}, //14
+	};
 	cout<<" type command\n";
 	string s;
 	getline(cin,s);
@@ -29,4 +48,39 @@ int main(){
 	if(s=="132"){
 		cout<<" left window is closed, the shades are open, right window is closed.\n";
 	}
+	for(int i=0; i<5;i++){
+		cout<<"hi\n";
+	}
+	cout<<' ';
+	cout<<v[0].first;
+	cout<<' ';
+	cout<<v[0].second;
+	cout<<' ';
+	cout<<v[1].first;
+	cout<<' ';
+	cout<<v[1].second;
+	cout<<' ';
+	cout<<v[2].first;
+	cout<<' ';
+	cout<<v[2].second;
+	cout<<' ';
+	cout<<v[3].first;
+	cout<<' ';
+	cout<<v[3].second;
+	cout<<' ';
+	cout<<v[4].first;
+	cout<<' ';
+	cout<<v[4].second;
+	cout<<' ';
+	cout<<v[5].first;
+	cout<<' ';
+	cout<<v[5].second;
+	cout<<' ';
+	cout<<v[6].first;
+	cout<<v[6].second;
+	cout<<v[7].first;
+	cout<<v[7].second;
+	cout<<v[8].first;
+	cout<<v[8].second;
+	
 }
