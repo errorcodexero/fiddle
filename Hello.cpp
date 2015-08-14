cba8e708ff308bb4592bc3650b427420131d2993
Software1 <nothing@nothin.com>
2015-08-13 20:47:24 -0700

    added a file it does tic tac toe

---
+++ Hello.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main (){
+	cout<<"choose a number between 1-9\n";
+	string x;
+	getline(cin,x);
+	int
+	cout<<x;
+	cout<<"\n";
+	cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxx\n";
+	cout<<"x        x        x        x\n";
+	cout<<"x        x        x        x\n";
+	cout<<"x        x        x        x\n";
+	cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxx\n";
+	cout<<"x        x        x        x\n";
+	cout<<"x        x        x        x\n";
+	cout<<"x        x        x        x\n";
+	cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxx\n";
+	cout<<"x        x        x        x\n";
+	cout<<"x        x        x        x\n";
+	cout<<"x        x        x        x\n";
+	cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxx\n";
+
+	return 0;
+}
+	
+int o(){
+
+
+}
