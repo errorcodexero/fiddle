00146b0332fb974f4f9a2da3fe1262d51dfd7742
rince_charming <nothing@nothin.com>
2015-09-03 19:54:28 -0700

    Editions to stas-gen.

--- Jacob/Jacob/stats-gen/stats-gen.cpp
+++ Jacob/Jacob/stats-gen/stats-gen.cpp
@@ -1,16 +1,55 @@
+#include<stdlib.h>
 #include<iostream>
 
 using namespace std;
 
+float find_lengths_per_game(float robot_travel_speed, float field_length){
+	float answer;
+	
+	return answer;
+}
+
 int main(){
-	int robot_travel_speed;
-	int robot_rotation_speed;
-	int pickup_speed;
-	int pass_speed;
-	int reload_time;
-	int accuracy;
-	int field_length = 54;
-	cout<<"\n\nTravel speed (ft/sec): ";
+	float robot_travel_speed;
+	float robot_rotation_speed;
+	float pickup_speed;
+	float pass_speed;
+	float reload_time;
+	float accuracy;
+	float field_length;
+	float field_width;
+	float total_game_time;
+	float auto_game_time;
+
+	string answer;
+	
+	field_length = 54;
+	field_width = 0;
+	//-----START-----
+	
+	system("clear");
+	cout<<"Current built-in settings: \n";
+	cout<<"   Field length (ft): "<<field_length;
+	cout<<"\n   Field width (ft): "<<field_width;
+	cout<<"\n\nDo you want to use built-in settings? (y/n): ";
+	cin>>answer;
+	cout<<"\n----------------------------------------------\n\n";
+	cout<<"Please fill in the following: \n\n";
+	
+	if(answer == "n"){
+		cout<<"\nField length (ft): ";
+		cin>>field_length;
+		cout<<"\nField width (ft): ";
+		cin>>field_width;
+		cout<<"\nTotal game time (sec. only): ";
+		cin>>total_game_time;
+		cout<<"\nAutonomus game time (sec. only): ;
+		cin>>
+	} else if(answer != "y"){
+		cout<<"\n\nError: 'y' or 'n' not selected.\n\n";
+		exit(1);
+	}
+	cout<<"\nTravel speed (ft/sec): ";
 	cin>>robot_travel_speed;
 	cout<<"\nRotation speed (rpm): ";
 	cin>>robot_rotation_speed;
@@ -22,7 +61,9 @@ int main(){
 	cin>>reload_time;
 	cout<<"\nAccuracy (%): ";
 	cin>>accuracy;
-	
-	
+
+	//find_lengths_per_game(robot_travel_speed, field_length);
+
+		
 	
 }
