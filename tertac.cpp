16de1bf1a125773bec94e2303adac1b0fe62dc2f
rince_charming <nothing@nothin.com>
2015-12-05 11:46:24 -0800

    update tertac

--- tertac.cpp
+++ tertac.cpp
@@ -44,6 +44,9 @@ string print(vector<Board> x, vector<Board> o){
 	return s;
 }
 Board stob(string s){
+	for(int i=0;i<s.size();i++){
+		s[i]=toupper(s[i]);
+	}
 	if(s=="UL")return Board::UL;
 	if(s=="UC")return Board::UC;
 	if(s=="UR")return Board::UR;
@@ -73,26 +76,36 @@ bool fin(vector<Board> v){
 	return false;
 }
 int main(){
-	vector<Board> x,o,a={Board::UL,Board::UC,Board::UR,Board::CL,Board::CC,Board::CR,Board::LL,Board::LC,Board::LR};
+	vector<Board> x,o;
+	bool status=0;
 	while(1){
 		cout<<print(x,o)<<endl<<"Player 1 input place on board: ";
 		string s;
 		cin>>s;
 		x.push_back(stob(s));
-		if(fin(x) ||fin(o))break;
+		if(fin(x) ||fin(o)){
+			status=true;
+			break;
+		}
 		vector<Board> v=x;
 		for(int i=0;i<o.size(); i++){
 			v.push_back(o[i]);
 		}
-		for(int i=0;i<v.size(); i++){
-			for(int j=0;j<a.size(); i++){
-				if(v[i]==a[j])NOT YET IMPLEMENTED!
-			}
-		}
+		if(v.size()==9)break;
 		cout<<print(x,o)<<endl<<"Player 2 input place on board: ";
 		cin>>s;
 		o.push_back(stob(s));
-		if(fin(x) ||fin(o))break;
+		if(fin(x) ||fin(o)){
+			status=true;
+			break;
+		}
+		v=x;
+		for(int i=0;i<o.size(); i++){
+			v.push_back(o[i]);
+		}
+		if(v.size()==9)break;
 	}
-	cout<<print(x,o)<<"\nCCCCCONGRATZZZZ!!!!!!11";
+	cout<<print(x,o)<<"\n";
+	if(status)cout<<"CCCCCONGRATZZZZ!!!!!!11\n";
+	else cout<<"YOU SUCK\n";
 }
