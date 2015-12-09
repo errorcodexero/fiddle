41cf8d1a896ab99c61d91985bc92c96d038c34da
Adrian-Chimera <earthvexingtimelord@gmail.com>
2015-12-08 19:43:12 -0800

    Scout-proto program

--- adrian/scouterproto.cpp
+++ adrian/scouterproto.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string>
-#include <math.h>
+#include <cmath>
+#include <fstream>
 
 using namespace std;
 
 struct Team{
-	int num,int average; 
+	//int num, average, match_num, alliance_score, match_penalties, defense, score;
+	int num, average, match_num;
+	string name; 
+	Team(){}
 };
 
 int main(){
-	
-	
+	Team test;	
+	return 0;
 }
