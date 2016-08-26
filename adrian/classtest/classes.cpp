88b0c9746b2820ea5715bbccb3a999d8dae1853b
rince_charming <nothing@nothin.com>
2016-08-25 19:37:28 -0700

    Added Adrian's class test

---
+++ adrian/classtest/classes.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+struct Animal{
+	virtual void make_noise()=0;
+	Animal(){}
+};
+	
+struct Cow:public Animal{
+	void make_noise(){
+		cout<<"oink"<<endl;
+	}
+	Cow():Animal(){}
+};
+
+struct Dog:public Animal{
+	bool angry;
+	void make_noise(){
+		if(angry)cout<<"hiss"<<endl;
+		else cout<<"meow"<<endl;
+	}
+	Dog():Animal(){	
+		angry=true;
+	}
+};
+
+int main(){
+	unique_ptr<Animal>a;
+	a=make_unique<Dog>();
+	a->make_noise();
+	return 0;
+}
+
