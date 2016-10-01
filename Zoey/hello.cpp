c1b6e870528e8f12113371414d1e630a84f4ef56
rince_charming <nothing@nothin.com>
2016-10-01 12:33:48 -0700

    more code

--- Zoey/hello.cpp
+++ Zoey/hello.cpp
@@ -1,6 +1,16 @@
+#include <string>
 #include <iostream>
 using namespace std;
 int main(){
-	cout<<"git test\n";
+	string name;
+	cout<<"please input your name:";
+	getline(cin,name);
+	if(name=="zoey")cout<<"yay\n";
+	else if(name=="mathew")cout<<"cat\n";
+	else if(name=="logan")cout<<"nerd\n";
+	else cout<<"nay\n";
+	while(true){
+		cout<<"nerd\n";
+	}
 	return 0;
 }
