461d9aaacdf527f36d2162df8ec26e713562120b
Wonderwoman <nothing@nothin.com>
2016-10-08 12:32:37 -0700

    Begin work on higher-lower game

--- Ethan/hi.cpp
+++ Ethan/hi.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout<<"hi"<<endl;
+	char postition;
+	int guess=50;
+	while(true){
+		cout<<"is it "<<guess<<"?(y,h,l)";
+		cin>>postition;
+		if(postition=='y')break;
+		if(postition=='h')guess = guess+guess/2;
+		if(postition=='l')guess = guess-guess/2;
+	}
+	return 0;
 }
+
