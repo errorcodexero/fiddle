ec7c48daabb7aaefd4e5efdc15f9d44b6d2ebd46
Software2 <1425sw@gmail.com>
2016-10-10 20:54:12 -0700

    start on main

--- Michellee/hello.cpp
+++ Michellee/hello.cpp
@@ -1,12 +1,26 @@
 #include<iostream>
 using namespace std;
-int main(){
-cout<<"Hello world!\n";
-int x=0;
-	while (x<5){
-	cin>>x;
+int main (){
+	string s;
+	s="b";
+	getline(cin,s);
+	cout<<s<<"\n";
+	//cout<<"Hello world!\n";
+	if(s=="1"){
+		cout<<"right,right,right!\n";
+	}
+	if(s=="5") {
+		cout<<"go left\n";
+	}	
+	if(s=="2"){
+		cout<<"two right\n";
+	}
+	if(s=="3"){
+		cout<<"one right\n";
+	}
+	if(s=="4"){
+		cout<<"stay\n";
 	}
 }
-//x=x*5;
-//cout<<x<<"\n";
+//if(left,left,left)
 
