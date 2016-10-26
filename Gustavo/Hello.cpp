920dd16101c355f298e9a11b228e701677b6bb02
Gold Drive Computer <goldenboy@aol.com>
2016-10-25 20:22:28 -0700

    Working on a Maze Game

--- Gustavo/Hello.cpp
+++ Gustavo/Hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream> 
 using namespace std; 
 
-int main(){
+/*int main(){
 	int x=0, y=0;
 	cout<<"Enter a number:";
 	cin>>x;
@@ -10,5 +10,20 @@ int main(){
 	if(x < y -5)cout<<"cat\n";
 	else cout<<"dog\n";
 
+}*/
+ 
+int main(){
+	int x=0, y=0;
+	cout<<"Please put a number where you want to go:";
+	//cin>>x;
+	cin>>x;
+	
+	if(x==4)cout<<"Do nothing";
+	if(x==3) cout<<"Go left";
+	if(x==2)cout<<"Go two times left";
+	if(x==1)cout<<"Go three times left";
+	if(x==0)cout<<"Go four times left";
+	if(x==5)cout<<"Go right";
+	
 }
  
