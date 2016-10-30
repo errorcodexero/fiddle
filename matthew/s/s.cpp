7c2ba98b0ba7172f4a4a3e1caf4ccaef0b074161
Matthew Macovsky <macovsky.m@gmail.com>
2016-10-29 17:01:21 -0700

    Now works

--- matthew/s/s.cpp
+++ matthew/s/s.cpp
@@ -17,19 +17,25 @@ int error(string msg) {
 size_t find_value(char* buffer, size_t size, size_t nmemb, Value_data* data) {
 	string page = buffer;
 	smatch match;
-	data->found = data->found || regex_search(page, match, regex("data bgLast\">(.*)</p>"));
-	data->multi_match = data->multi_match || match.size() > 2;
-	//if (data->found && !data->multi_match) data->value = match[1];
+	bool found = regex_search(page, match, regex("data bgLast\">(.*)</p>"));
+	bool multi_match = match.size() > 2;
+	data->found = data->found || found;
+	data->multi_match = data->multi_match || multi_match;
+	if (found && !multi_match) data->value = match[1];
 	return nmemb;	
 }
 
-int main() {
+int main(int argc, char** argv) {
 	Value_data data;
+	if (argc < 2) error("A stock symbol must be supplied as an argument.");
+	string symbol(argv[1]);
+	string ss("http://www.marketwatch.com/investing/stock/" + symbol);
+
 	CURL *curl = curl_easy_init();
 	if (!curl) return error("CURL pointer was not initialized.");
-	
+
 	CURLcode res;
-	curl_easy_setopt(curl, CURLOPT_URL, "http://www.marketwatch.com/investing/stock/AAPL");
+	curl_easy_setopt(curl, CURLOPT_URL, ss.c_str());
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, find_value);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
 	res = curl_easy_perform(curl);
