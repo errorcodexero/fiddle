bdb169aa72d40f5285a6e44b6809fa2b0b5191a4
rince_charming <nothing@nothin.com>
2016-12-10 11:52:19 -0800

    fixed bugs in coding

--- Michellee/hello.cpp
+++ Michellee/hello.cpp
@@ -6,8 +6,6 @@ int get_num(){
 	getline (cin,f);
 	int s=atoi (f.c_str());
 	return s;
-	cout<<"f is equal to "<<f<<"\n";
-	//exit(5);//
 }
 int main (){
 	
@@ -48,19 +46,21 @@ int main (){
 		cout<<"go right four\n";
 	}
 	if (difference==-5){
-		cout<<"go right five\n";
+	out<<"go right five\n";
 	}*/
-cout<< "Where in the \"y\" axis are you now (0-5)?\n";
+	cout<< "Where in the \"y\" axis are you now (0-5)?\n";
 	int current_y_value=get_num();
 	cout<< "Where in the \"y\" axis do you want to be (0-5)?\n";	
 	int target_2_wanted_y_value=get_num();
 	int difference_y = current_y_value -target_2_wanted_y_value;
-	int at;
+	pair<int,int> at;
+	at.first=current_x_value ;
+	at.second=current_y_value;
 	int target;
-	while (at!=target){
-		if (at>target){
+	while (at.first!=target_2_wanted_x_value || at.second!=target_2_wanted_y_value){
+		if (at.first >target){
 			cout<< "go left\n";
-			at --;
+			at.first--;
 			cout<<"Go left"<<difference_x<<"\n";
 		}
 	}
