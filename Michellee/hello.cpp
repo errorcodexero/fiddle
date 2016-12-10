e6d45ba86b3606267a59e974c461b57fa678b589
rince_charming <nothing@nothin.com>
2016-12-10 15:06:06 -0800

    fixed bugs

--- Michellee/hello.cpp
+++ Michellee/hello.cpp
@@ -14,40 +14,7 @@ int main (){
 	cout<< "Where in the \"x\" axis do you want to be (0-5)?\n";	
 	int target_2_wanted_x_value=get_num();
 	int difference_x = current_x_value -target_2_wanted_x_value;
-	/*cout<<"difference: 0"<<difference<<"\n";
-	if (difference==0){
-		cout<<"stay\n";
-	}
-	if (difference==1){
-		cout<<"go left\n";
-	}
-	if (difference==2){
-		cout<<"go left two\n";
-	}
-	if (difference==3){
-		cout<<"go left three\n";
-	}
-	if (difference==4){
-		cout<<"go left four\n";
-	}
-	if (difference==5){
-		cout<<"go left five\n";
-	}
-	if (difference==-1){
-		cout<<"go right one\n";
-	} 
-	if (difference==-2){
-		cout<<"go right two\n";
-	} 
-	if (difference==-3){
-		cout<<"go right three\n";
-	}
-	if (difference==-4){
-		cout<<"go right four\n";
-	}
-	if (difference==-5){
-	out<<"go right five\n";
-	}*/
+	
 	cout<< "Where in the \"y\" axis are you now (0-5)?\n";
 	int current_y_value=get_num();
 	cout<< "Where in the \"y\" axis do you want to be (0-5)?\n";	
@@ -59,47 +26,10 @@ int main (){
 	int target;
 	while (at.first!=target_2_wanted_x_value || at.second!=target_2_wanted_y_value){
 		if (at.first >target){
-			cout<< "go left\n";
+			cout<< "Go left\n";
 			at.first--;
-			cout<<"Go left"<<difference_x<<"\n";
 		}
 	}
-
-	/*cout<<"difference: 0"<<difference<<"\n";
-	if (difference==0){
-		cout<<"stay\n";
-	}
-	if (difference==1){
-		cout<<"go left\n";
-	}
-	if (difference==2){
-		cout<<"go left two\n";
-	}
-	if (difference==3){
-		cout<<"go left three\n";
-	}
-	if (difference==4){
-		cout<<"go left four\n";
-	}
-	if (difference==5){
-		cout<<"go left five\n";
-	}
-	if (difference==-1){
-		cout<<"go right one\n";
-	} 
-	if (difference==-2){
-		cout<<"go right two\n";
-	} 
-	if (difference==-3){
-		cout<<"go right three\n";
-	}
-	if (difference==-4){
-		cout<<"go right four\n";
-	}
-	if (difference==-5){
-		cout<<"go right five\n";
-	}*/
-
 }
 
 
