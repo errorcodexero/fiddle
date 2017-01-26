5b4f834fb37187926811dd50c6025dfdede29b98
rince_charming <nothing@nothin.com>
2017-01-25 20:53:26 -0800

    added array# Please enter the commit message for your changes. Lines starting

--- Krysztof/project.cpp
+++ Krysztof/project.cpp
@@ -7,16 +7,25 @@ using namespace std;
 
 struct point {
 	int x, y;
-}start, end;
-struct maze2d {
-};	
+}start, end;	
+const int X_LENGTH = 10, Y_LENGTH = 10;
+void print_maze(point start, point end){	
+		for(int y = 0; y<Y_LENGTH; y++){
+			for(int x = 0; x<X_LENGTH; x++){
+				if(y == start.y && x == start.x) cout<<"S";
+				else if (y ==end.y && x == end.x) cout<<"E";
+				else cout<<"X";
+			}	
+	}
+}
 int main () {
 	srand(time(0));
-	start.x=rand()%10;
-	start.y=rand()%10;
-	end.x=rand()%10;
-	end.y=rand()%10;
+	start.x=rand()%X_LENGTH;
+	start.y=rand()%X_LENGTH;
+	end.x=rand()%X_LENGTH;
+	end.y=rand()%X_LENGTH;
 	cout<<"Start"<<" "<<start.x<<","<<start.y<<endl;
+	print_maze(start,end);
 	if (start.x<end.x) {
 		cout<<"Go right"<<" "<<(end.x-start.x)<<" "<<"times."<<endl;
 	} 
