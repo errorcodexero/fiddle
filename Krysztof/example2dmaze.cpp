d1ed9d6fb35df47baee9370abf700005f157f953
Software4 <1425sw@gmail.com>
2017-01-26 20:31:53 -0800

    added new project

---
+++ Krysztof/example2dmaze.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+using namespace std;
+
+struct point {
+	int x, y;
+};
+
+const int X_LENGTH = 20, Y_LENTGH = 20;
+
+int wall[] ={0,1};
+
+int main() {
+	srand(time(0));
+	return 0;
+}
