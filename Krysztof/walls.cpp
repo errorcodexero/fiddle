814d71d5a7efded1f5cc791bd36c19d65747d155
Software2 <1425sw@gmail.com>
2017-02-13 20:38:39 -0800

    Set it so starting and ending points can't be set inside wall. Need to implement better solver to avoid walls:w

--- Krysztof/walls.cpp
+++ Krysztof/walls.cpp
@@ -18,48 +18,8 @@ point a() {
 	return z;
 }
 vector <point> visited;
-vector <point> find_path(point p1, point p2) {
-	
-	if (p1.x<p2.x) {
-		for (int i=p1.x; i<p2.x; i++) {
-			point p;
-			p.x=i;
-			p.y=p1.y;
-			visited.push_back(p);
-		}
-		cout<<"Go right "<<(p2.x-p1.x)<<" times"<<endl;
-	}
-	else if (p1.x>p2.x) {
-		for (int i=p1.x; i>p2.x; i--) {
-			point p;
-			p.x=i;
-			p.y=p1.y;
-			visited.push_back(p);
-		}
-		cout<<"Go left "<<(p1.x-p2.x)<<" times"<<endl;
-	}
-	if (p1.y<p2.y) {
-		for (int i=p1.y; i<p2.y; i++) {
-			point p;
-			p.y=i;
-			p.x=p2.x;
-			visited.push_back(p);
-		}
-		cout<<"Go down "<<(p2.y-p1.y)<<" times"<<endl;
-	}
-	else if (p1.y>p2.y) {
-		for (int i=p1.y; i>p2.y; i--) {
-			point p;
-			p.y=i;
-			p.x=p2.x;
-			visited.push_back(p);
-		}
-		cout<<"Go up "<<(p1.y-p2.y)<<" times"<<endl;
-	}
-	return visited;
-}
-
-void print_maze(point start,point mid, point end, vector <point> walls, vector <point> visited){	
+vector <point> walls;
+void print_maze(point start, point end, vector <point> walls, vector <point> visited){	
 	for(int y = 0; y<Y_LENGTH; y++){
 		string row="";
 		for(int x = 0; x<X_LENGTH; x++){		
@@ -86,27 +46,77 @@ void print_maze(point start,point mid, point end, vector <point> walls, vector <
 						iswall=true;
 					}
 				}
-				if (unvisited && !iswall)row+=" ";
-
+				else if (unvisited && !iswall)row+=" ";
+			
 			}
 		}
 		cout<<row<<endl;
 	}
 }
 int main () {
-	point start, mid, end;
-	vector <point> walls;
+	point start, end;
 	srand(time(0));
 	for (int x = 2; x<X_LENGTH;x++) {
 		walls.push_back({x,5});
+		start=a();
+		end=a();
+		while (start.y==5) {
+			if (start.y==5) {
+				start=a();
+			}
+			else break;
+		}
+		while (end.y==5) {
+			if (end.y==5) {
+				end=a();
+			}
+			else break;
+		}
 	}
-	start=a();
-	mid=a();
-	end=a();
 	cout<<"Start"<<" "<<start.x<<","<<start.y<<endl;
-	find_path(start,mid);
-	find_path(mid,end); 
+	find_path(start,end);
+vector <point> find_path(point p1, point p2) {
+	if (p1.x<p2.x) {
+		for (int i=p1.x; i<p2.x; i++) {
+			point p;
+			p.x=i;
+			p.y=p1.y;
+			visited.push_back(p);
+		}
+		cout<<"Go right "<<(p2.x-p1.x)<<" times"<<endl;
+	}
+	else if (p1.x>p2.x) {
+		for (int i=p1.x; i>p2.x; i--) {
+			point p;
+			p.x=i;
+			p.y=p1.y;
+			visited.push_back(p);
+		}
+		cout<<"Go left "<<(p1.x-p2.x)<<" times"<<endl;
+	}
+	if (p1.y<p2.y) {
+		if (p1.y!= walls.y) {
+			for (int i=p1.y; i<p2.y; i++) {
+				point p;
+				p.y=i;
+				p.x=p2.x;
+				visited.push_back(p);
+			}
+		}
+		cout<<"Go down "<<(p2.y-p1.y)<<" times"<<endl;
+	}
+	else if (p1.y>p2.y) {
+		for (int i=p1.y; i>p2.y; i--) {
+			point p;
+			p.y=i;
+			p.x=p2.x;
+			visited.push_back(p);
+		}
+		cout<<"Go up "<<(p1.y-p2.y)<<" times"<<endl;
+	}
+	return visited;
+}
 	cout<<"End"<<" "<<end.x<<","<<end.y<<endl;
-		print_maze(start,mid,end,walls,visited);
+		print_maze(start,end,walls,visited);
 	return 0;
 }
