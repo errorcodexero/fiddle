f58ae34ad0dbdb4b58c5a1af4d0505cb25730db6
Software2 <1425sw@gmail.com>
2017-09-21 19:46:21 -0700

    hello

--- Kaden/hello.cpp
+++ Kaden/hello.cpp
@@ -1,9 +1,19 @@
+/*The include command tells the program to include a package needed for some commands*/
 #include <iostream>
+/*using namespace std; tells the program to output cout*/
 using namespace std;
+/*int main() is the entry point once the program is run*/
 int main(){
+	/*cout echoes its content to the screen*/
+	/*endl and \n are used as line breaks*/
 	cout<<"Hello Kaden!"<<endl;
+	/*int is used to tell the variable ahead of it to recognise it as an 		integer*/
 	int x=10;
+	/*<< is used to seperate text/variable outputs*/
+	cout<<"multiple "<<"item "<<"text "<<endl;
 	cout<<x<<endl;
+	cout<<"hello!\n";
+	/*return tells the program to exit the current stream*/
 	return 0;
 }
 
--- Krysztof/walls.cpp
+++ Krysztof/walls.cpp
@@ -19,62 +19,6 @@ point a() {
 }
 vector <point> visited;
 vector <point> walls;
-void print_maze(point start, point end, vector <point> walls, vector <point> visited){	
-	for(int y = 0; y<Y_LENGTH; y++){
-		string row="";
-		for(int x = 0; x<X_LENGTH; x++){		
-			if(y == start.y && x == start.x) {
-				row+="S";
-			}
-			else if (y ==end.y && x == end.x) {
-				row+="E";
-			}
-			else {
-				bool unvisited=true;
-				for(unsigned i=0; i<visited.size(); i++) {
-					point p=visited[i];					
-					if(y==p.y && x==p.x) {
-						row+="-";
-						unvisited=false;
-					}
-				}
-				bool iswall=false;
-				for(unsigned i=0; i<walls.size(); i++) {
-					point p=walls[i];					
-					if(y==p.y && x==p.x) {
-						row+="x";
-						iswall=true;
-					}
-				}
-				else if (unvisited && !iswall)row+=" ";
-			
-			}
-		}
-		cout<<row<<endl;
-	}
-}
-int main () {
-	point start, end;
-	srand(time(0));
-	for (int x = 2; x<X_LENGTH;x++) {
-		walls.push_back({x,5});
-		start=a();
-		end=a();
-		while (start.y==5) {
-			if (start.y==5) {
-				start=a();
-			}
-			else break;
-		}
-		while (end.y==5) {
-			if (end.y==5) {
-				end=a();
-			}
-			else break;
-		}
-	}
-	cout<<"Start"<<" "<<start.x<<","<<start.y<<endl;
-	find_path(start,end);
 vector <point> find_path(point p1, point p2) {
 	if (p1.x<p2.x) {
 		for (int i=p1.x; i<p2.x; i++) {
@@ -116,6 +60,61 @@ vector <point> find_path(point p1, point p2) {
 	}
 	return visited;
 }
+void print_maze(point start, point end, vector <point> walls, vector <point> visited){	
+	for(int y = 0; y<Y_LENGTH; y++){
+		string row="";
+		for(int x = 0; x<X_LENGTH; x++){		
+			if(y == start.y && x == start.x) {
+				row+="S";
+			}
+			else if (y ==end.y && x == end.x) {
+				row+="E";
+			}
+			else {
+				bool unvisited=true;
+				for(unsigned i=0; i<visited.size(); i++) {
+					point p=visited[i];					
+					if(y==p.y && x==p.x) {
+						row+="-";
+						unvisited=false;
+					}
+				}
+				bool iswall=false;
+				for(unsigned i=0; i<walls.size(); i++) {
+					point p=walls[i];					
+					if(y==p.y && x==p.x) {
+						row+="x";
+						iswall=true;
+					}
+				}
+				else if (unvisited && !iswall)row+=" ";
+			}
+		}
+		cout<<row<<endl;
+	}
+}
+int main () {
+	point start, end;
+	srand(time(0));
+	for (int x = 2; x<X_LENGTH;x++) {
+		walls.push_back({x,5});
+		start=a();
+		end=a();
+		while (start.y==5) {
+			if (start.y==5) {
+				start=a();
+			}
+			else break;
+		}
+		while (end.y==5) {
+			if (end.y==5) {
+				end=a();
+			}
+			else break;
+		}
+	}
+	cout<<"Start"<<" "<<start.x<<","<<start.y<<endl;
+	find_path(start,end);
 	cout<<"End"<<" "<<end.x<<","<<end.y<<endl;
 		print_maze(start,end,walls,visited);
 	return 0;
